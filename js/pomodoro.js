a22b2fc9eb9fd5df6bcd0636a54d0194faf7c4f6
farr16 <msamfarr@outlook.com>
2017-04-30 14:33:25 -0700

    Add Countdown Related Variables
    Adds variables for defining work period length and break period length
    in minutes, as well as countdown duration in seconds.
    
    Assigns values to interface elements based on those elements.
    
    Adds a helper function for converting countdown seconds into hh:mm:ss
    for clock display.

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,7 +1,34 @@
+var workTime;
+var breakTime;
+var countSec;
+
 $(document).ready(function() {
-  $('#work-display').text('25');
-  $('#break-display').text('5');
+  // Define variables used for keeping track of work and break sessions
+  workTime = 25;
+  breakTime = 5;
+  countSec = workTime * 60;
+  
+  // Set interface component texts
+  $('#work-display').text(workTime);
+  $('#break-display').text(breakTime);
   $('#session-display').text('WORK');
-  $('#clock-display').text('00:25:00');
+  setClockSeconds(countSec);
   $('#timer-button').text('START');
 });
+
+/* Helper function which takes the countdown seconds used by the timer function and converts it into a human readable
+ * hh:mm:ss format and displays it on the clock
+ */
+function setClockSeconds(seconds) {
+  var hrs = Math.trunc(seconds/3600);
+  if (hrs < 10)
+	hrs = '0' + hrs;
+  seconds %= 3600;
+  var min = Math.trunc(seconds/60);
+  if (min < 10)
+	min = '0' + min;
+  seconds %= 60;
+  if (seconds < 10)
+	seconds = '0' + seconds;
+  $('#clock-display').text(hrs + ':' + min + ':' + seconds);
+}
