c29fcd0ba5a64b2e9c2394dcf9335899e7ba7505
farr16 <msamfarr@outlook.com>
2017-04-30 11:03:57 -0700

    Add JavaScript Interface Code
    Adds javascript directory, with a file inside called pomodoro.js.
    Includes code for setting interface text programmatically instead of
    statically in index.html

---
+++ js/pomodoro.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#work-display').text('25');
+  $('#break-display').text('5');
+  $('#session-display').text('WORK');
+  $('#clock-display').text('00:25:00');
+  $('#timer-button').text('START');
+});
